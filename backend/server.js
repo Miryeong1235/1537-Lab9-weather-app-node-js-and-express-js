cb87e4a803d20fc91028bc61e1bc5f8cd59229ce
Misuzu Taniguchi <me.tpn23@gmail.com>
2023-11-17 16:41:55 -0800

    Delete redundant code

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,6 @@ const app = express();
 const cors = require('cors');
 app.use(cors()); //I am enabling te
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// })
-
-// app.use(express.urlencoded({ extended: true }))
 app.get('/getCityWeather', async (req, res) => {
     resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.cname}&appid=c0cc8f065c4ca0234b6fc2b84f8f2833&units=metric`);
     jsonResp = await resp.json();
